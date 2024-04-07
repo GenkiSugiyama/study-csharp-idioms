7a03770a0825cf294cc477f154b1acc41d8e972a
GenkiSugiyama <sgym0318marcos@gmail.com>
2024-04-07 18:17:44 +0900

    chapter2 practice2-1

---
+++ Chapter2/Practice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Chapter2.Practice // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Song[] songs = [
+                new Song("うたうた", "artist1", 40),
+                new Song("へいへいへい", "artist2", 30),
+                new Song("うたばん", "artist3", 60)
+            ];
+
+            foreach (var (song, index) in songs.Select((song, index) => (song, index)))
+            {
+                var timeSpan = new TimeSpan(0, 0, song.Length);
+                Console.WriteLine($"{index + 1}曲目");
+                Console.WriteLine($"タイトル：{song.Title}");
+                Console.WriteLine($"歌手：{song.ArtistName}");
+                Console.WriteLine($"演奏時間：{timeSpan.ToString(@"mm\:ss")}");
+                Console.WriteLine("----------");
+            }
+        }
+    }
+
+    public class Song
+    {
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+        public Song(string title, string artistName, int length)
+        {
+            this.Title = title;
+            this.ArtistName = artistName;
+            this.Length = length;
+        }
+    }
+}
