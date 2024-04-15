f9b4771003519ea58c24f29a15fe22addf2f7845
GenkiSugiyama <sgym0318marcos@gmail.com>
2024-04-16 00:08:31 +0900

    done 5-1

--- Chapter5/Practice/Program.cs
+++ Chapter5/Practice/Program.cs
@@ -3,6 +3,17 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        var str1 = Console.ReadLine();
+        var str2 = Console.ReadLine();
 
+        // 5-1
+        if(String.Compare(str1, str2, ignoreCase: true) == 0)
+        {
+            Console.WriteLine($"5-1: str1:{str1}とstr2:{str2}は一致しています");
+        }
+        else
+        {
+            Console.WriteLine($"5-1: str1:{str1}とstr2:{str2}は不一致です");
+        }
     }
 }
